4e948ee7a46305f5abe772ed6264d5cfd13f5e91
Raul Romo <rromomxdev@gmail.com>
2021-04-30 09:38:23 -0500

    fmt: data container

--- src/components/DataContainer.js
+++ src/components/DataContainer.js
@@ -2,14 +2,14 @@ import { Table, Tbody } from '@chakra-ui/table'
 import React, { useEffect, useState } from 'react'
 import DataItem from './DataItem'
 
-const useFetchData = (page) => {  
+const useFetchData = (page) => {
   const [users, setUsers] = useState([])
 
-  useEffect(() => {   
+  useEffect(() => {
     async function fetchData() {
       const response = await fetch(`https://randomuser.me/api/?page=${page}&results=30&seed=abc`)
       const json = await response.json()
-      
+
       setUsers(oldUsers => {
         return [...new Set([...oldUsers, ...json.results])]
       })
@@ -20,16 +20,16 @@ const useFetchData = (page) => {
   return users
 }
 
-const DataTable = ({info}) => {
+const DataTable = ({ info }) => {
   return (
     <>
       <Table>
         <Tbody>
           {info?.map(item => (
-            <DataItem key={`${item.email}`} data={item}/>
+            <DataItem key={`${item.email}`} data={item} />
           ))}
-        </Tbody>        
-      </Table>      
+        </Tbody>
+      </Table>
     </>
   )
 }
@@ -40,15 +40,15 @@ const DataContainer = () => {
 
   function handleScroll(event) {
     const val = event.target
-    
+
     if (val.scrollHeight - val.scrollTop === val.clientHeight) {
       setPage(oldPage => oldPage + 1)
     }
   }
-  
+
   return (
-    <div h={1000} onScroll={handleScroll} style={{overflowY: 'scroll'}}>
-      {users && <DataTable info={users}/>}
+    <div h={1000} onScroll={handleScroll} style={{ overflowY: 'scroll' }}>
+      {users && <DataTable info={users} />}
     </div>
   )
 }
